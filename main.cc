82240a05fc77524e90da54fd6955eb888016d9fa
yousaf_1004564 <nayousaf@gmail.com>
2025-11-03 15:07:13 -0800

    added our names

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 //Fill out this comment with your names and which bullet points you did
-//Partners:
+//Partners: Mckay, Nicola, Nabil
 //Bullet Points: 
 //Extra Credit: 
 //URL to cover art and music: 
